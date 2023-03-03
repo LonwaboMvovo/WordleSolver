58b3d372da6d5b335e3b697739b7e8b53661ca21
lomvovo022 <lomvovo022@wethinkcode.co.za>
2023-03-03 11:22:21 +0200

    moved main file to solver and changed main file to first check if requests is installed before referencing solver

--- main.py
+++ main.py
@@ -1,148 +1,16 @@
-import requests
-import random
+import pkg_resources
+import subprocess
 
+from sys import executable
 
-def get_words():
-    # S/O https://gist.github.com/iancward/afe148f28c5767d5ced7a275c12816a3
-    # S/0 https://github.com/tabatkins/wordle-list
+if __name__ == '__main__':
+    # Make sure requests installed
+    required = {'requests'}
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
 
-    # Get list of 5 letter english words from meaningpedia.com
-    wordle_word_list_resp = requests.get("https://raw.githubusercontent.com/tabatkins/wordle-list/main/words")
-    return wordle_word_list_resp.text.split("\n")
+    if missing:
+        python = executable
+        subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
 
-
-def check_letters(placed_letters, current_guess):
-    new_invalid_letters = ""
-
-    franken_word = ["", "", "", "", ""]
-
-    letter_colours = input("\nWhat is the order of colours (b - black, y - yellow, g - green): ").replace(" ", "").lower()
-
-    while any(letter not in "byg" for letter in letter_colours) or len(letter_colours) != 5:
-        print("\nInvalid input! Please input a string with 5 characters representing the colours that the given letters got, e.x. \"bbygb\"")
-        letter_colours = input("What is the order of colours (b - black, y - yellow, g - green): ").replace(" ", "").lower()
-
-    for franken_index in range(5):
-        letter = current_guess[franken_index]
-        letter_colour = letter_colours[franken_index]
-
-        if letter_colour == "g":
-            placed_letters += letter
-
-        if letter_colour == "b" and letter not in placed_letters:
-            new_invalid_letters += letter
-
-        franken_word[franken_index] = letter_colour + letter
-
-    return new_invalid_letters, franken_word, placed_letters
-
-
-def remove_invalid_words(word_list, invalid_letters, franken_word):
-    # remove words containing invalid letters
-    for word in word_list.copy():
-        removed_words = []
-
-        for letter in invalid_letters:
-            if word not in removed_words and letter in word:
-                word_list.remove(word)
-                removed_words.append(word)
-
-    # remove words that don't have green letters
-    for word in word_list.copy():
-        removed_words = []
-
-        for letter_index in range(len(franken_word)):
-            letter_colour = franken_word[letter_index][0]
-            letter = franken_word[letter_index][1]
-
-            if ((word not in removed_words) and
-                    (letter_colour == "g" and word[letter_index] != letter)):
-                word_list.remove(word)
-                removed_words.append(word)
-
-    # remove words that don't have yellow letters
-    for word in word_list.copy():
-        removed_words = []
-
-        for letter_index in range(len(franken_word)):
-            letter_colour = franken_word[letter_index][0]
-            letter = franken_word[letter_index][1]
-
-            if ((word not in removed_words) and
-                    (letter_colour == "y" and letter not in word)):
-                word_list.remove(word)
-                removed_words.append(word)
-
-    # remove words that don't have green letters
-    for word in word_list.copy():
-        removed_words = []
-
-        for letter_index in range(len(franken_word)):
-            letter_colour = franken_word[letter_index][0]
-            letter = franken_word[letter_index][1]
-
-            if ((word not in removed_words) and
-                    (letter_colour == "y" and word[letter_index] == letter)):
-                word_list.remove(word)
-                removed_words.append(word)
-    return word_list
-
-
-def survey_says(franken_word, current_guess, num_tries, all_guesses, word_list):
-    playing_game = True
-
-    all_guesses.append(current_guess)
-
-    if all("g" == colour_letter[0] for colour_letter in franken_word):
-        print("\n*****\nnoice\n*****\n")
-        print(f"It took {6-num_tries} guesses and there {'were' if len(word_list) > 1 else 'was'} {len(word_list)} {'words' if len(word_list) > 1 else 'word'} left to choose from:")
-        playing_game = False
-    elif not num_tries or not len(word_list):
-        print("\n*********\nnot noice\n*********\n")
-        print(f"There were {len(word_list)} {'words' if len(word_list) > 1 else 'word'} left to choose from:")
-        playing_game = False    
-    return playing_game, num_tries - 1, all_guesses
-    
-
-if __name__ == "__main__":
-    word_list = get_words()
-    placed_letters = ""
-    invalid_letters = ""
-    all_guesses = []
-
-    num_tries = 5
-
-    # First guess
-    # A pair of MIT researchers said its the best starting word idk
-    current_guess = "salet"
-    print("According to a pair of MIT researchers \"salet\" is statistically the best word to start with...\n")
-    print(f"So, lets try \"{current_guess}\"")
-
-    while input("Is the word valid(y/n)? ").lower() == "n":
-            word_list.remove(current_guess)
-            current_guess = random.choice(word_list)
-            print(f"\nOK then lets try {current_guess}")
-
-    invalid_letters, franken_word, placed_letters = check_letters(placed_letters, current_guess)
-    word_list = remove_invalid_words(word_list, invalid_letters, franken_word)
-    playing_game, num_tries, all_guesses = survey_says(franken_word, current_guess, num_tries, all_guesses, word_list)
-
-    # TODO to clean up this do while loop can make word_list only have "salet" at first then when num_tries is 5 can use the full word list
-    while playing_game:
-        print("Ahh, unluck!")
-
-        current_guess = random.choice(word_list)
-        print(f"\nLets try {current_guess}")
-
-        while input("Is the word valid(y/n)? ").lower() == "n":
-            word_list.remove(current_guess)
-            current_guess = random.choice(word_list)
-            print(f"\nOK then lets try {current_guess}")
-
-        invalid_letters, franken_word, placed_letters = check_letters(placed_letters, current_guess)
-        word_list = remove_invalid_words(word_list, invalid_letters, franken_word)
-        playing_game, num_tries, all_guesses = survey_says(franken_word, current_guess, num_tries, all_guesses, word_list)
-
-    print(word_list)
-
-    print("\nThese were all the guesses:\n",all_guesses)
+    import solver
